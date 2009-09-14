af73fad94d8c3a7d215431507abd31e79a037559
Charlie <root@toshiba.home.paul-brian.com>
2009-09-14 16:51:39 +0100

    changes to the pkg builder

--- osbuilder/build/os/portbuilder/mk_pkgs.sh
+++ osbuilder/build/os/portbuilder/mk_pkgs.sh
@@ -5,16 +5,11 @@ FTPSVR=ftp.office.pirc.co.uk
 PIRCTMP=/usr/local/PIRC/tmp/$UNTAR
 REPO_DIR=/usr/ports/packages/All
 
-### This is manually generated for now.
-### need to parse Makefile in ports but read porter handbook chp 5 - bit complex
-PKG_LIST="apache-2.2.11_4 apr-gdbm-db42-1.3.3.1.3.4_1 bash-4.0.10_2 \
-git-1.6.2.2 mysql-client-5.1.33 python-2.5,2 py25-MySQLdb-1.2.2 \
-py25-lxml-2.2 subversion-1.6.0_2 sudo-1.6.9.20"
-
-
 
 PKG_LIST=""
-PORTS_LIST="lang/python devel/py-lxml shells/bash sysutils/screen devel/subversion security/sudo www/apache22 devel/git databases/mysql51-server databases/mysql51-client databases/py-MySQLdb"
+PORTS_LIST="lang/python devel/py-lxml shells/bash sysutils/screen \
+devel/subversion security/sudo www/apache22 devel/git \
+databases/mysql51-server databases/mysql51-client databases/py-MySQLdb"
 
 
 
