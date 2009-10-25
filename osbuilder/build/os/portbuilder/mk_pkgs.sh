88b0675aa39cbf10db9cfcdf74a161c1f7a7e324
Charlie <root@toshiba.home.paul-brian.com>
2009-10-25 19:50:55 +0000

    changes to overview.txt

--- osbuilder/build/os/portbuilder/mk_pkgs.sh
+++ osbuilder/build/os/portbuilder/mk_pkgs.sh
@@ -1,29 +1,30 @@
+#!/bin/sh
+
+## Author: pbrian@itmanagerscookbook.com
+
 ############################### USER VARIABLES ###############################
-UNTAR=untar
-VERSION=freebsd-7.1.0
-FTPSVR=ftp.office.pirc.co.uk
-PIRCTMP=/usr/local/PIRC/tmp/$UNTAR
 REPO_DIR=/usr/ports/packages/All
+BOOTSTRAP=/tmp/bootstrap_barebones.sh
 
 
 PKG_LIST=""
-PORTS_LIST="lang/python devel/py-lxml shells/bash sysutils/screen \
-devel/subversion security/sudo www/apache22 devel/git \
-databases/mysql51-server databases/mysql51-client databases/py-MySQLdb"
+PORTS_LIST="lang/python devel/py-lxml shells/bash sysutils/screen "
+#\
+#devel/subversion security/sudo www/apache22 devel/git \
+#databases/mysql51-server databases/mysql51-client databases/py-MySQLdb"
 
 
 
 ##############################################################################
 
-echo "=> Starting pkg building"
 mkdir -p $REPO_DIR
 
 for PORT in $PORTS_LIST
 do
 
-cd /usr/ports/$PORT
-PKG=`make package-name`
-echo "==> discover $PORT is $PKG"
+ cd /usr/ports/$PORT
+ PKG=`make package-name`
+ echo "==> discover $PORT is $PKG"
 
 # visit the mirror of All, and crete packages recursively
 # I could use make package-recursive but that then *builds* the ports too
@@ -33,9 +34,13 @@ pkg_create -R -b $PKG
 ## keep the pkg names
 PKG_LIST=$PKG_LIST\ $PKG
 
+
+echo \$PKG_ADD \$FTP_TO_REPO/$PKG >> $BOOTSTRAP
+
 done
 
 echo "pkg created "
 echo \# $PORTS_LIST >> pkg_create.log
 echo $PKG_LIST >> pkg_create.log
+echo $PKG_LIST
 
